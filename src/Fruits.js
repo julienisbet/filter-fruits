3b92f89df52341d3fc05690cd3b26b9426e8a45a
harriet-the-spy <dpcairns@gmail.com>
2021-06-08 13:17:49 -0700

    add filter dropdown

--- src/Fruits.js
+++ src/Fruits.js
@@ -32,26 +32,35 @@ const fruits = [
 ]
 export default class Counter extends Component {
     state = {
+        filterCategory: ''
     }
 
     handleFruitChange = (e) => {
-        this.setState({ fruit: e.target.value })
+        // e.target.value is the VALUE of whatever we clicked on
+        this.setState({ filterCategory: e.target.value })
     }
-
 
     render() {
         // cool zone -- this is a nice place to do work.
+        // this.state.fruit can be one of three things: apple, citrus, banana
+        let filteredFruits = fruits;
+
+        if (this.state.filterCategory) {
+             filteredFruits = fruits.filter(fruit => fruit.category === this.state.filterCategory)
+        }
+        
         return (
             <div>
                 <label>
                     <select onChange={this.handleFruitChange}>
+                       <option value="">All Fruits</option>
                         <option value="apple">Apple</option>
                         <option value="citrus">Citrus</option>
                         <option value="banana">Banana</option>
                     </select>
                 </label>
                 {
-                    fruits.map(fruit => <p>{fruit.name} is a {fruit.category}</p>)
+                    filteredFruits.map(fruit => <p>{fruit.name} is a {fruit.category}</p>)
                 }
             </div>
         )
